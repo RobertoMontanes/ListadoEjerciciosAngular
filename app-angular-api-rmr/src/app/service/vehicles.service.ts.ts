5f50763dbaaa92a83f2ea2ae91ec2818eb3b6bb8
Roberto <montanes.rarob24@triana.salesianos.edu>
2025-11-17 12:42:08 +0100

    modificacion

--- app-angular-api-rmr/src/app/service/vehicles.service.ts.ts
+++ app-angular-api-rmr/src/app/service/vehicles.service.ts.ts
@@ -5,8 +5,9 @@ import { VehiclesResponse } from '../models/VehiclesResponse';
 
 @Injectable({ providedIn: 'root' })
 export class VehiclesServiceTs {
-  constructor(private http: HttpClient) {}
+  private apiUrl = 'https://swapi.dev/api/vehicles/';
 
+  constructor(private http: HttpClient) {}
   getVehicleList(): Observable<VehiclesResponse> {
     return this.http.get<VehiclesResponse>('https://swapi.dev/api/vehicles/');
   }
