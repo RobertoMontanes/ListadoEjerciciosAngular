8c6deef653f189e5330ba78a78f2cd1047692407
Roberto <rmr15092004@gmail.com>
2025-11-17 08:58:17 +0100

    Commit random

--- app-angular-formulario-rmr/src/app/student-new-page/student-new-page.ts
+++ app-angular-formulario-rmr/src/app/student-new-page/student-new-page.ts
@@ -11,23 +11,17 @@ export class StudentNewPage {
   profileForm = new FormGroup({
     nombre: new FormControl('',[
       Validators.required,
-      Validators.minLength(2),
-      Validators.maxLength(50),
-      Validators.pattern(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/)
+      
     ]),
     edad: new FormControl('', [
       Validators.required,
-      Validators.pattern(/^[0-9]{8}[A-Z]$|^[XYZ][0-9]{7}[A-Z]$/)
     ]),
     telefono: new FormControl('', [
       Validators.required,
-      Validators.pattern(/^[0-9]{8}[A-Z]$|^[XYZ][0-9]{7}[A-Z]$/)
     ]),
     curso: new FormControl('',[
       Validators.required,
-      Validators.minLength(2),
-      Validators.maxLength(50),
-      Validators.pattern(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/)
+      
     ]),
   });
   onSubmit() {
