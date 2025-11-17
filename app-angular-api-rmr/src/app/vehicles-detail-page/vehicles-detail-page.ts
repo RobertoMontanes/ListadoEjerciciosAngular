99635f7e70aa7912234a4531154b38ebd6bf3e63
Roberto <montanes.rarob24@triana.salesianos.edu>
2025-11-17 13:59:36 +0100

    Entregado sin cambios

--- app-angular-api-rmr/src/app/vehicles-detail-page/vehicles-detail-page.ts
+++ app-angular-api-rmr/src/app/vehicles-detail-page/vehicles-detail-page.ts
@@ -8,16 +8,13 @@ import { CommonModule } from '@angular/common';
   selector: 'app-vehicles-detail-page',
   imports: [CommonModule],
   templateUrl: './vehicles-detail-page.html',
-  styleUrls: ['./vehicles-detail-page.css']
+  styleUrls: ['./vehicles-detail-page.css'],
 })
 export class VehiclesDetailPage implements OnInit {
   vehicle: VehicleDetail | null = null;
-  loading: boolean = true;
 
-  constructor(
-    private route: ActivatedRoute,
-    private vehiclesService: VehiclesServiceTs
-  ) {}
+  constructor(private route: ActivatedRoute, 
+              private vehiclesService: VehiclesServiceTs) {}
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
@@ -25,12 +22,7 @@ export class VehiclesDetailPage implements OnInit {
       this.vehiclesService.getVehicle(+id).subscribe({
         next: (data) => {
           this.vehicle = data;
-          this.loading = false;
         },
-        error: (error) => {
-          console.error('Error fetching vehicle:', error);
-          this.loading = false;
-        }
       });
     }
   }
