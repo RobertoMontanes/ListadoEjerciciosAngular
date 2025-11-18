9a19eb723bc5d0685cb4383c39b4b57e88571492
Roberto <montanes.rarob24@triana.salesianos.edu>
2025-11-18 12:59:06 +0100

    crear pryecot

--- CineMatchRecommender/src/app/app.spec.ts
+++ CineMatchRecommender/src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, app-angular-api-rmr');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, CineMatchRecommender');
   });
 });
--- CineMatchRecommender/src/app/app.ts
+++ CineMatchRecommender/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('app-angular-api-rmr');
+  protected readonly title = signal('CineMatchRecommender');
 }
