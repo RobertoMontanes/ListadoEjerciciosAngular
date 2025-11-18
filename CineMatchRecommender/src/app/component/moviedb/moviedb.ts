63555841b8671e5a53ef8793995a32a71baa871e
Roberto <montanes.rarob24@triana.salesianos.edu>
2025-11-18 13:22:59 +0100

    Actualización paquetes

---
+++ CineMatchRecommender/src/app/component/moviedb/moviedb.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-moviedb',
+  imports: [],
+  templateUrl: './moviedb.html',
+  styleUrl: './moviedb.css',
+})
+export class Moviedb {
+
+}
--- CineMatchRecommender/src/app/service/movidedb.ts
+++ CineMatchRecommender/src/app/service/movidedb.ts
@@ -1,8 +1,21 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Movidedb {
-  
+  private API_TOKEN = 'f52455191bdf4f07b1e601ef89258db2';
+  private API_URL = 'https://api.themoviedb.org/3';
+
+  constructor(private http: HttpClient) {}
+
+  getPopularMovies(): Observable<any> {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${this.API_TOKEN}`
+    });
+
+    return this.http.get(`${this.API_URL}/movie/popular`, { headers });
+  }
 }
